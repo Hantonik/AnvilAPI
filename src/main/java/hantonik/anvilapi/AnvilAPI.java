d6c8409952a32d05f41df9a6901fc74a7ee821ca
Piotr Matusiewicz <piotrmatusiewicz85@gmail.com>
2022-10-13 22:08:24 +0200

    Added log info

--- src/main/java/hantonik/anvilapi/AnvilAPI.java
+++ src/main/java/hantonik/anvilapi/AnvilAPI.java
@@ -1,28 +1,40 @@
 package hantonik.anvilapi;
 
-import hantonik.anvilapi.init.RecipeSerializers;
-import hantonik.anvilapi.init.Recipes;
+import hantonik.anvilapi.init.AARecipeSerializers;
+import hantonik.anvilapi.init.AARecipeTypes;
+import hantonik.atomic.core.utils.references.AtomicReferences;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(AnvilAPI.MOD_ID)
 public final class AnvilAPI {
     public static final String MOD_ID = "anvilapi";
+    public static final String MOD_NAME = "AnvilAPI";
+
+    public static final Logger LOGGER = LogManager.getLogger(AnvilAPI.MOD_NAME);
 
     public AnvilAPI() {
+        AnvilAPI.LOGGER.info(AtomicReferences.SETUP_MARKER, "Initializing...");
+
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         bus.register(this);
 
-        Recipes.RECIPES.register(bus);
-        RecipeSerializers.SERIALIZERS.register(bus);
+        AARecipeTypes.TYPES.register(bus);
+        AARecipeSerializers.SERIALIZERS.register(bus);
     }
 
     @SubscribeEvent
     public void commonSetup(final FMLCommonSetupEvent event) {
+        AnvilAPI.LOGGER.info(AtomicReferences.SETUP_MARKER, "Starting common setup...");
+
         MinecraftForge.EVENT_BUS.register(this);
+
+        AnvilAPI.LOGGER.info(AtomicReferences.SETUP_MARKER, "Finished common setup!");
     }
 }
