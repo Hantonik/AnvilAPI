9ed10a334fa5aa8d12068d462ad289a107c64f05
Piotr Matusiewicz <piotrmatusiewicz85@gmail.com>
2022-10-13 22:07:33 +0200

    Renamed from `Recipes`

---
+++ src/main/java/hantonik/anvilapi/init/AARecipeTypes.java
@@ -0,0 +1,23 @@
+package hantonik.anvilapi.init;
+
+import hantonik.anvilapi.api.recipe.IAnvilRecipe;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public final class AARecipeTypes {
+    public static final DeferredRegister<RecipeType<?>> TYPES = DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, "minecraft");
+
+    public static final RegistryObject<RecipeType<IAnvilRecipe>> ANVIL = register("anvil");
+
+    private static <T extends Recipe<?>> RegistryObject<RecipeType<T>> register(final String name) {
+        return TYPES.register(name, () -> new RecipeType<>() {
+            @Override
+            public String toString() {
+                return name;
+            }
+        });
+    }
+}
