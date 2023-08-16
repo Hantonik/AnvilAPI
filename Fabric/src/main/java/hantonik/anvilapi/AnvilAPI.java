56b04925f376fb884461d37976f4ec402d2fbb67
Hantonik <piotrmatusiewicz85@gmail.com>
2023-08-16 02:47:33 +0200

    Added Fabric support

---
+++ Fabric/src/main/java/hantonik/anvilapi/AnvilAPI.java
@@ -0,0 +1,20 @@
+package hantonik.anvilapi;
+
+import hantonik.anvilapi.init.AARecipeSerializers;
+import hantonik.anvilapi.init.AARecipeTypes;
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class AnvilAPI implements ModInitializer {
+    public static final String MOD_NAME = "AnvilAPI";
+    public static final Logger LOGGER = LogManager.getLogger(AnvilAPI.MOD_NAME);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing...");
+
+        AARecipeTypes.onInit();
+        AARecipeSerializers.onInit();
+    }
+}
