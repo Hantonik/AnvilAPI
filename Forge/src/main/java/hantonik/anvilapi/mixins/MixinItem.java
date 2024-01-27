fc237047c6e93e31dabf8fa1cac5f9206442ba93
Hantonik <piotrmatusiewicz85@gmail.com>
2024-01-27 02:29:55 +0100

    Fix custom repair recipes not working

---
+++ Forge/src/main/java/hantonik/anvilapi/mixins/MixinItem.java
@@ -0,0 +1,23 @@
+package hantonik.anvilapi.mixins;
+
+import hantonik.anvilapi.init.AARecipeTypes;
+import hantonik.anvilapi.utils.AARecipeHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.flag.FeatureElement;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.common.extensions.IForgeItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public abstract class MixinItem implements FeatureElement, ItemLike, IForgeItem {
+    @Inject(at = @At("HEAD"), method = "isValidRepairItem", cancellable = true)
+    public void isValidRepairItem(ItemStack stack, ItemStack repairCandidate, CallbackInfoReturnable<Boolean> callback) {
+        if (AARecipeHelper.getRecipeManager().getRecipeFor(AARecipeTypes.ANVIL_REPAIR.get(), new SimpleContainer(stack, repairCandidate), null).isPresent())
+            callback.setReturnValue(true);
+    }
+}
