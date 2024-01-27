000fedbe7ea07252cba36727387a36b19c3b7ff0
Hantonik <piotrmatusiewicz85@gmail.com>
2024-01-28 00:28:19 +0100

    [NeoForge] Hotfix crash without Architectury

--- NeoForge/src/main/java/hantonik/anvilapi/mixins/MixinItem.java
+++ NeoForge/src/main/java/hantonik/anvilapi/mixins/MixinItem.java
@@ -1,6 +1,5 @@
 package hantonik.anvilapi.mixins;
 
-import dev.architectury.extensions.ItemExtension;
 import hantonik.anvilapi.init.AARecipeTypes;
 import hantonik.anvilapi.utils.AARecipeHelper;
 import net.minecraft.world.SimpleContainer;
@@ -8,13 +7,14 @@ import net.minecraft.world.flag.FeatureElement;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.neoforged.neoforge.common.extensions.IItemExtension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Item.class)
-public abstract class MixinItem implements FeatureElement, ItemLike, ItemExtension {
+public abstract class MixinItem implements FeatureElement, ItemLike, IItemExtension {
     @Inject(at = @At("HEAD"), method = "isValidRepairItem", cancellable = true)
     public void isValidRepairItem(ItemStack stack, ItemStack repairCandidate, CallbackInfoReturnable<Boolean> callback) {
         if (AARecipeHelper.getRecipeManager().getRecipeFor(AARecipeTypes.ANVIL_REPAIR.get(), new SimpleContainer(stack, repairCandidate), null).isPresent())
